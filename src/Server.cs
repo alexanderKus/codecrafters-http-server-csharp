34a43e311a81cd7d31addc7f9d032f7846a8fbd7
Alesander Kus <79520681+alexanderKus@users.noreply.github.com>
2025-01-27 20:39:16 +0100

    Task3

--- src/Server.cs
+++ src/Server.cs
@@ -1,32 +1,10 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using codecrafters_http_server;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
-
-var buffer = new byte[4096];
-TcpListener server = new(IPAddress.Any, 4221);
+Console.WriteLine("APPLICATION START");
+var server = new HttpServer();
 server.Start();
-using var socket = server.AcceptSocket();
-var bytesRead = socket.Receive(buffer);
-var request = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-var httpParts = request.Split("\r\n").AsSpan();
-var httpInfo = httpParts[0].Split(" ").AsSpan();
-var headers = httpParts[1..];
-var httpMethod = httpInfo[0];
-var httpPath = httpInfo[1];
-var httpVersion = httpInfo[2];
-var message = string.Empty;
-if (httpPath == "/")
-{
-    Console.WriteLine("Success");
-    message = "HTTP/1.1 200 OK\r\n\r\n";
-}
-else
-{
-    Console.WriteLine("Not Found");
-    message = "HTTP/1.1 404 Not Found\r\n\r\n";
-}
-
-socket.Send(Encoding.UTF8.GetBytes(message));
+server.Handle();
+Console.WriteLine("APPLICATION END");
