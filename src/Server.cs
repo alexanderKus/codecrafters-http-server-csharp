f5a5d76add6bed115ae85c1b273e3c4682768e57
Alesander Kus <79520681+alexanderKus@users.noreply.github.com>
2025-01-27 20:06:29 +0100

    Task3

--- src/Server.cs
+++ src/Server.cs
@@ -5,8 +5,28 @@ using System.Text;
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
-TcpListener server = new TcpListener(IPAddress.Any, 4221);
+var buffer = new byte[4096];
+TcpListener server = new(IPAddress.Any, 4221);
 server.Start();
-var socket = server.AcceptSocket(); // wait for client
-socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n"));
+using var socket = server.AcceptSocket();
+var bytesRead = socket.Receive(buffer);
+var request = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+var httpParts = request.Split("\r\n").AsSpan();
+var httpInfo = httpParts[0].Split(" ").AsSpan();
+var headers = httpParts[1..];
+var httpMethod = httpInfo[0];
+var httpPath = httpInfo[1];
+var httpVersion = httpInfo[2];
+var message = string.Empty;
+if (httpPath == "/")
+{
+    Console.WriteLine("Success");
+    message = "HTTP/1.1 200 OK\r\n\r\n";
+}
+else
+{
+    Console.WriteLine("Not Found");
+    message = "HTTP/1.1 404 Not Found\r\n\r\n";
+}
+
+socket.Send(Encoding.UTF8.GetBytes(message));
